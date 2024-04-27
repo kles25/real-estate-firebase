f11face8f15444842a4e6daecbf63f88d1084be2
Lester John Rodney Intia <i.lesterjohnrodney@gmail.com>
2024-04-28 02:53:15 +0800

    save

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -4,7 +4,7 @@ import { getAuth } from "firebase/auth";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-    apiKey: `${import.meta.env.REACT_APP_FIREBASE_KEY}`,
+    apiKey: `${import.meta.env.VITE_FIREBASE_KEY}`,
     authDomain: "real-estate-1c673.firebaseapp.com",
     projectId: "real-estate-1c673",
     storageBucket: "real-estate-1c673.appspot.com",
@@ -12,8 +12,6 @@ const firebaseConfig = {
     appId: "1:461133790278:web:8964a818f3145b52f05e47",
     measurementId: "G-6D7X8JZMNQ"
 };
-
-
 
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
